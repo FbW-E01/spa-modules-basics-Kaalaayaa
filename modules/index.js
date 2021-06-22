ec55a086272ef4219748d3ac7b9d2ac648a6dd0a
Kaalaayaa <kalaya.chatelain@yahoo.fr>
2021-06-22 13:36:40 +0200

    done

---
+++ modules/index.js
@@ -0,0 +1,7 @@
+import { clickImages } from "/modules/click.js";
+import { hoverHeader } from "/modules/hover.js";
+import { scrollWindow } from "/modules/scroll.js";
+
+clickImages();
+hoverHeader();
+scrollWindow();
